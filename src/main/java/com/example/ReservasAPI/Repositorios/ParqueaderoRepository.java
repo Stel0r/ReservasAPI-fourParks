f1b272a3d0aa77dbb1dc19b1cc898b5980828328
Diego Gamez <diego.felipe.gamez@gmail.com>
2024-05-06 22:07:37 -0500

    update metodo extra

--- src/main/java/com/example/ReservasAPI/Repositorios/ParqueaderoRepository.java
+++ src/main/java/com/example/ReservasAPI/Repositorios/ParqueaderoRepository.java
@@ -16,7 +16,7 @@ import jakarta.persistence.Column;
 public interface ParqueaderoRepository extends CrudRepository<Parqueadero,String>{    
     public List<Parqueadero> findBycodCiudad(String ciudad);
 
-    @Query(value = "SELECT p.K_COD_PARQUEADERO,p.N_NOMBRE,p.I_ESTADO,p.I_24Hrs,p.F_HORA_APERTURA,p.F_HORA_CIERRE,I_FIDELIZACION,p.O_DIRECCION,p.O_LATITUD,p.O_LONGITUD,p.K_COD_GERENTE,p.K_COD_TIPO_PARQ,c.K_NOMBRE_CIUDAD,p.K_COD_TARIFA,p.N_NUM_PUESTOS from CIUDAD c,Parqueadero p WHERE p.K_COD_CIUDAD = c.K_COD_CIUDAD", nativeQuery = true)
+    @Query(value = "SELECT p.K_COD_PARQUEADERO as codParqueadero,p.N_NOMBRE as nombre,p.I_ESTADO as iEstado,p.I_24Hrs as i24Hrs,p.F_HORA_APERTURA horaApertura,p.F_HORA_CIERRE as horaCierre,I_FIDELIZACION iFidelizacion,p.O_DIRECCION as direccion,p.O_LATITUD as latitud,p.O_LONGITUD as longitud,p.K_COD_GERENTE as codGerente,p.K_COD_TIPO_PARQ tipoParqueadero,c.K_NOMBRE_CIUDAD ciudad,p.K_COD_TARIFA codTarifa,p.N_NUM_PUESTOS numPuestos from CIUDAD c,Parqueadero p WHERE p.K_COD_CIUDAD = c.K_COD_CIUDAD", nativeQuery = true)
     public List<Map<String,Object>> encontrarParqueaderos();
 }
 
