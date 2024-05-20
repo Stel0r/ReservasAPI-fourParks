af6d97e94ac1a0546e76ecb43e4cd5d67908ccf4
Diego Gamez <65517739+Stel0r@users.noreply.github.com>
2024-05-20 13:05:33 -0500

    cambio endpoint reservas

--- src/main/java/com/example/ReservasAPI/Controllers/ReservasController.java
+++ src/main/java/com/example/ReservasAPI/Controllers/ReservasController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,7 +34,8 @@ class InterfazCreacionReserva{
 }
 
 @CrossOrigin
-@RestController("/reservas")
+@RestController
+@RequestMapping("/reservas")
 public class ReservasController {
 
     @Autowired
