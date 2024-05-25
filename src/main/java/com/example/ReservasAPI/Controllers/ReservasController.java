4108b8b9b81873d6d68dcceb9c75f4d12fdc7831
Diego Gamez <65517739+Stel0r@users.noreply.github.com>
2024-05-25 16:34:55 -0500

    Update ReservasController.java

--- src/main/java/com/example/ReservasAPI/Controllers/ReservasController.java
+++ src/main/java/com/example/ReservasAPI/Controllers/ReservasController.java
@@ -68,7 +68,6 @@ public class ReservasController {
             reservasRepository.registrarReserva(body.idReserva,Date.valueOf(body.fechaReserva), Time.valueOf(body.tiempoInicio), Time.valueOf(body.tiempoFinal), body.tipoVehiculo, BigInteger.valueOf(body.numDocumento), body.tipoDoc, body.codParqueadero,request.getRemoteAddr(), body.subTotal);
             return ResponseEntity.ok().body(Map.of("Response","Se ha creado la nueva reserva"));
         } catch (Exception e) {
-            System.out.println(e.getMessage());
             return ResponseEntity.badRequest().body(Map.of("Response",e.getMessage().split("##")[1]));
         }
     }
