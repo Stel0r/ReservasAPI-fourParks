cac3cbe150fcc199b3115fac23509d4f395b6feb
Diego Gamez <diego.felipe.gamez@gmail.com>
2024-05-26 22:57:23 -0500

    Update Auditoria.java

--- src/main/java/com/example/ReservasAPI/Logica/Auditoria.java
+++ src/main/java/com/example/ReservasAPI/Logica/Auditoria.java
@@ -7,7 +7,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "Auditoria")
+@Table(name = "AUDITORIA")
 public class Auditoria {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
